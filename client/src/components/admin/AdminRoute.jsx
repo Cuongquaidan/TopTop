14c4e185b2d8d6096e43cc80ae7f8897a155b805
Hà Mạnh Cường <hamanhcuong18112004@gmail.com>
2025-04-18 20:09:23 +0700

    axios

--- client/src/components/admin/AdminRoute.jsx
+++ client/src/components/admin/AdminRoute.jsx
@@ -1,10 +1,12 @@
 import React from 'react'
 import LayoutAdmin from './LayoutAdmin'
 import { Outlet } from 'react-router-dom'
+import { ToastContainer } from 'react-toastify'
 
 function AdminRoute() {
   return (
     <div>
+      <ToastContainer></ToastContainer>
       <LayoutAdmin>
         <Outlet></Outlet>
       </LayoutAdmin>
